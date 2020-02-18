6dfcb4bf1aca3961e15b211b9bb2f17a59f8e0e1
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2020-02-18 18:12:15 +0900

    skip populate if values are empty

--- app/concepts/author/contract/create.rb
+++ app/concepts/author/contract/create.rb
@@ -9,7 +9,13 @@ module Author::Contract
     validates :age , presence: true
 
     books_prepopulator = ->(options){ books.append(Book.new) while books.size < 3 }
-    collection :books, prepopulator: books_prepopulator, populate_if_empty: Book do
+    books_populator = ->(options){ 
+      if options[:fragment].values.all?(&:blank?)
+        return skip!
+      end
+      books.append(Book.new)
+    }
+    collection :books, prepopulator: books_prepopulator, populator: books_populator do
       property :name
     end
   end
