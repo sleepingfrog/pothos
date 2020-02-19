37c98e6d675779c2bae5a60eb262f68a294a220e
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2020-02-19 17:15:36 +0900

    lock!

--- app/concepts/author/operation/update.rb
+++ app/concepts/author/operation/update.rb
@@ -1,12 +1,16 @@
 module Author::Operation
   class Update < Trailblazer::Operation
     step Wrap( ->(options, &block){ Author.transaction do block.call end }){
-      step Model(Author, :find)
+      step :set_model!
       step Contract::Build(constant: Author::Contract::Edit)
       step Contract::Validate(key: "author")
       step :persist!
       fail :prepopulate!
     }
+
+    def set_model!(options, *)
+      options["model"] = Author.lock.find(options[:params][:id])
+    end
 
     def persist!(options, *)
       contract = options["contract.default"]
