120d57345f8b0e5cdc33d70e69517b8aa70a33c1
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2020-02-20 16:25:10 +0900

    .gitignore

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -2,6 +2,6 @@ require "shrine"
 require "shrine/storage/file_system"
 
 Shrine.storages = {
-  candy_wrap_cache: Shrine::Storage::FileSystem.new("public", prefix: "candy_wrap/uploads/cache"),
-  candy_wrap_store: Shrine::Storage::FileSystem.new("public", prefix: "candy_wrap/uploads/store"),
+  candy_wrap_cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/candy_wrap/cache"),
+  candy_wrap_store: Shrine::Storage::FileSystem.new("public", prefix: "uploads/candy_wrap/store"),
 }
