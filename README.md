1d26d13506261e7a78f7f63fcd21bef3ed5c6469
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2020-02-21 04:15:58 +0900

    minio

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ dip provision
 ### depends
 + docker-compose
 + dip
+
+## minio
+s3互換を手元で動かして見たかったのでminioを使ったが
+ホストマシンのhostsに
+```txt
+127.0.0.1   minio
+```
+を追加しないといい感じに動作しない・・・どうしたものか・・・
--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,7 +1,16 @@
 require "shrine"
-require "shrine/storage/file_system"
+require "shrine/storage/s3"
+
+s3_options = {
+  access_key_id: ENV["S3_ACCESS_KEY"],
+  secret_access_key: ENV["S3_SECRET_ACCESS_KEY"],
+  bucket: "pothos",
+  endpoint: ENV["S3_ENDPOINT"],
+  force_path_style: true,
+  region: "s3",
+}
 
 Shrine.storages = {
   candy_wrap_cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/candy_wrap/cache"),
-  candy_wrap_store: Shrine::Storage::FileSystem.new("public", prefix: "uploads/candy_wrap/store"),
+  candy_wrap_store: Shrine::Storage::S3.new(prefix: "uploads/candy_wrap/store", **s3_options),
 }
