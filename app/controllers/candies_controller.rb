86e6428a8259fa9121dbfa71282bf1ca7c53c8b3
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2020-02-20 16:19:17 +0900

    create read

--- app/controllers/candies_controller.rb
+++ app/controllers/candies_controller.rb
@@ -1,13 +1,27 @@
 class CandiesController < ApplicationController
   def index
+    @candies = Candy.all
   end
 
   def new
+    @candy = Candy.new
   end
 
   def create
+    @candy = Candy.new(candy_params)
+    if @candy.save
+      redirect_to candy_path(@candy)
+    else
+      render :new
+    end
   end
 
   def show
+    @candy = Candy.find(params[:id])
+  end
+
+  private
+  def candy_params
+    params.require(:candy).permit(:name, :price, :wrap_paper)
   end
 end
