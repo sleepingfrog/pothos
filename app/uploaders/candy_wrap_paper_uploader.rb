17f50c5a1cd68db1b4dbd8d3b830ef677844b449
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2020-02-21 02:53:20 +0900

    pdf to jpg

--- app/uploaders/candy_wrap_paper_uploader.rb
+++ app/uploaders/candy_wrap_paper_uploader.rb
@@ -4,7 +4,25 @@ class CandyWrapPaperUploader < Shrine
   plugin :derivatives
 
   Attacher.derivatives do |original|
+    case file.mime_type
+    when "image/jpeg"
+      process_derivatives(:image, original)
+    when "application/pdf"
+      process_derivatives(:pdf, original)
+    end
+  end
+
+  Attacher.derivatives :image do |original|
     magick = ImageProcessing::MiniMagick.source(original)
+
+    {
+      large: magick.resize_to_limit!(800, 800),
+      small: magick.resize_to_limit!(300, 300),
+    }
+  end
+
+  Attacher.derivatives :pdf do |original|
+    magick = ImageProcessing::MiniMagick.source(original).loader(page: 0).convert("jpg")
 
     {
       large: magick.resize_to_limit!(800, 800),
