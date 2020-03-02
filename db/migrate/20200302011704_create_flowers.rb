fa6fb8b1a3ae270693057393f6ba000b23d818f8
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2020-03-02 10:17:33 +0900

    root model

---
+++ db/migrate/20200302011704_create_flowers.rb
@@ -0,0 +1,10 @@
+class CreateFlowers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :flowers do |t|
+      t.string :name
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
