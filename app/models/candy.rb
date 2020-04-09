1e8c6c7d30c4edc3317b24bd0d159ac93b2bc7a7
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2020-04-09 16:17:46 +0900

    before save ?

--- app/models/candy.rb
+++ app/models/candy.rb
@@ -1,7 +1,7 @@
 class Candy < ApplicationRecord
   include CandyWrapPaperUploader::Attachment(:wrap_paper)
 
-  after_save do
+  before_save do
     wrap_paper_derivatives! if wrap_paper.present?
   end
 end
