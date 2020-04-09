5718954be39e7c48fb8c7ca7516fcb63cf97e1df
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2020-04-09 16:37:11 +0900

    201

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,17 +6,20 @@ class JobsController < ApplicationController
     7.times do
       AoiJob.perform_later
     end
+    head(201) and return
   end
 
   def itadori
     11.times do
       ItadoriJob.perform_later
     end
+    head(201) and return
   end
 
   def ume
     13.times do
       UmeJob.perform_later
     end
+    head(201) and return
   end
 end
