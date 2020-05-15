fe808ef06ae9d1d4bd22603dac19b3a81c5ec80d
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2020-05-15 18:43:12 +0900

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,3 +27,23 @@ end
 
   a.save!
 end
+
+
+
+suzumushi = Insect.create!(name: 'suzumushi')
+kamakiri = Insect.create!(name: 'kamakiri')
+kirigirisu = Insect.create!(name: 'kirigirisu')
+
+insects = [
+  suzumushi,
+  kamakiri,
+  kirigirisu,
+]
+
+insects.permutation.with_index(1) do |_insects, i|
+  h = Hotel.new(name: "hotel#{i}")
+  h.food = _insects[0]
+  h.staff = _insects[1]
+  h.guest = _insects[2]
+  h.save!
+end
