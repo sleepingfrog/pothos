834837545f56e8be5b9d4ca102ef455143dea6fb
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2020-05-15 18:25:55 +0900

    change migration file

--- db/migrate/20200515092313_create_hotels.rb
+++ db/migrate/20200515092313_create_hotels.rb
@@ -2,9 +2,9 @@ class CreateHotels < ActiveRecord::Migration[6.0]
   def change
     create_table :hotels do |t|
       t.string :name
-      t.references :staff, null: false, foreign_key: true
-      t.references :guest, null: false, foreign_key: true
-      t.references :food, null: false, foreign_key: true
+      t.references :staff, null: false, foreign_key: { to_table: :insects }
+      t.references :guest, null: false, foreign_key: { to_table: :insects }
+      t.references :food, null: false, foreign_key: { to_table: :insects }
 
       t.timestamps
     end
