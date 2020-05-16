4de41e3c7140990be8d1fd9ebea556cbb4997e61
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2020-05-16 12:44:44 +0900

    hotels search

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,5 +1,12 @@
 class HotelsController < ApplicationController
   def index
-    @hotels = Hotel.all
+    @search = Form::SearchHotel.new search_hotel_params
+    @hotels = @search.search
+    puts @hotels.explain
+  end
+
+  private
+  def search_hotel_params
+    params.fetch(:form_search_hotel, {}).permit(:food, :guest, :staff)
   end
 end
