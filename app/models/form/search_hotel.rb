3e3194cee9173395afb2b4df24d1adda4435c058
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2020-05-16 13:34:40 +0900

    まとめる

--- app/models/form/search_hotel.rb
+++ app/models/form/search_hotel.rb
@@ -26,58 +26,34 @@ module Form
       send(query_method, scope, value)
     end
 
-    def build_query_food(scope, value)
-      if value.blank?
-        return scope
+    %w[food staff guest].each do |key|
+      define_method "build_query_#{key}" do |scope, value|
+        build_insect_search_query(key, scope, value)
       end
-      food_table = Insect.arel_table.alias('foods')
-      value_string = "%#{value}%"
-      select_arel = scope.arel_table
-
-      join_source = scope
-        .arel
-        .join(food_table, Arel::Nodes::InnerJoin).on(select_arel[:food_id].eq(food_table[:id]))
-        .join_sources
-
-      scope
-        .joins(join_source)
-        .where(food_table[:name].matches(value_string, nil, true))
     end
 
-    def build_query_staff(scope, value)
+    def build_insect_search_query(key, scope, value)
       if value.blank?
         return scope
       end
-      staff_table = Insect.arel_table.alias('staffs')
-      value_string = "%#{value}%"
+
+      association = scope.model.reflect_on_association(key)
+      klass = association.klass
+      table = klass.arel_table.alias(key.to_s.pluralize)
+      foreign_key = association.foreign_key
       select_arel = scope.arel_table
 
+      value_string = "%#{value}%"
       join_source = scope
         .arel
-        .join(staff_table, Arel::Nodes::InnerJoin).on(select_arel[:staff_id].eq(staff_table[:id]))
+        .join(table, Arel::Nodes::InnerJoin).on(
+          select_arel[foreign_key].eq(table[klass.primary_key])
+        )
         .join_sources
 
       scope
         .joins(join_source)
-        .where(staff_table[:name].matches(value_string))
-    end
-
-    def build_query_guest(scope, value)
-      if value.blank?
-        return scope
-      end
-      guest_table = Insect.arel_table.alias('guests')
-      value_string = "%#{value}%"
-      select_arel = scope.arel_table
-
-      join_source = scope
-        .arel
-        .join(guest_table, Arel::Nodes::InnerJoin).on(select_arel[:guest_id].eq(guest_table[:id]))
-        .join_sources
-
-      scope
-        .joins(join_source)
-        .where(guest_table[:name].matches(value_string, nil, true))
+        .where(table[:name].matches(value_string, nil, true))
     end
   end
 end
